7f836f0640da2f8f3200e81283c69f83253dec3b
titustortoiseturtle1999 <ngtitus@ymail.com>
2021-09-29 09:40:55 +0800

    add pin declarations for motors

---
+++ ASV_ballshooter_1.0/ASV_ballshooter_1.0.ino
@@ -0,0 +1,60 @@
+//###################################################
+//###################################################
+//
+//####     ####
+//#  #     #  #      ######  ######## ########
+//#  ####  #  ####   #    ## #  ##  # #  ##  #
+//#     ## #     ##  ####  # #  ##  # #  ##  #
+//#  ##  # #  ##  # ##     # #  ##  # #  ##  #
+//#  ##  # #  ##  # #  ##  # #  ##  # ##    ##
+//#     ## #     ## ##     # ##     #  ##  ##
+// # ####   # ####   #######  #######   ####    
+//
+//
+//BBASV 3.0 ball shooter board
+//Firmware Version :  v1.0
+//
+// Written by Titus for AUV 3.0 Ball shooter 
+//
+//###################################################
+//###################################################
+
+// Stepper setup 
+const int stepper1_step = 2;
+const int stepper1_dir = 3;
+const int stepper2_step = 4;
+const int stepper2_dir = 5;
+const int stepper3_step = 6;
+const int stepper3_dir = 7;
+const int stepper4_step = 8;
+const int stepper4_dir = 9;
+const int stepper5_step = 10;
+const int stepper5_dir = 11;
+
+const int stepper1_m0 = 22;
+const int stepper1_m1 = 23;
+const int stepper1_m2 = 24;
+const int stepper2_m0 = 25;
+const int stepper2_m1 = 26;
+const int stepper2_m2 = 27;
+const int stepper3_m0 = 28;
+const int stepper3_m1 = 29;
+const int stepper3_m2 = 30;
+const int stepper4_m0 = 31;
+const int stepper4_m1 = 32; 
+const int stepper4_m2 = 33;
+const int stepper5_m0 = 34;
+const int stepper5_m1 = 35;
+const int stepper5_m2 = 36;
+
+// servo setup 
+const int servo_pwm = 12;
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
