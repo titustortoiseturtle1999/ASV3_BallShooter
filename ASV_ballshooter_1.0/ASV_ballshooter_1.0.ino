9ba17ec336c5d0fc5277586f45b0901422ca08a3
Titus Ng <ngtitus@ymail.com>
2022-04-04 22:05:28 +0800

    update dev & initial commit for v1.0 ballshooter code

--- ASV_ballshooter_1.0/ASV_ballshooter_1.0.ino
+++ ASV_ballshooter_1.0/ASV_ballshooter_1.0.ino
@@ -11,51 +11,160 @@
 // # ####   # ####   #######  #######   ####    
 //
 //
-//BBASV 3.0 ball shooter board
+//BBASV 3.0 ball shooter board prototype 1
 //Firmware Version :  v1.0
 //
 // Written by Titus for AUV 3.0 Ball shooter 
 //
-// TMC 2209 drivers are connected to Serial 1 
+// Test code for cocking motors on stepper 3 and 4 slot
+// Stepper 2 & 3: Cocking motors 
+// Stepper 4: Drum steppers 
+// Servo 1: Firing servo 
+// Servo 2: Drum continuous servo 
+// 
 //###################################################
 //###################################################
 
-// Stepper setup 
-const int stepper1_step = 2;
-const int stepper1_dir = 3;
-const int stepper2_step = 4;
-const int stepper2_dir = 5;
-const int stepper3_step = 6;
-const int stepper3_dir = 7;
-const int stepper4_step = 8;
-const int stepper4_dir = 9;
-const int stepper5_step = 10;
-const int stepper5_dir = 11;
+#include <Servo.h> 
+#include "define.h"
 
-const int stepper1_m0 = 22;
-const int stepper1_m1 = 23;
-const int stepper1_m2 = 24;
-const int stepper2_m0 = 25;
-const int stepper2_m1 = 26;
-const int stepper2_m2 = 27;
-const int stepper3_m0 = 28;
-const int stepper3_m1 = 29;
-const int stepper3_m2 = 30;
-const int stepper4_m0 = 31;
-const int stepper4_m1 = 32; 
-const int stepper4_m2 = 33;
-const int stepper5_m0 = 34;
-const int stepper5_m1 = 35;
-const int stepper5_m2 = 36;
+Servo fire;
+Servo drum;
 
-// servo setup 
-const int servo_pwm = 12;
+bool is_locked;
+
 void setup() {
-  // put your setup code here, to run once:
+  // Declare pins as output:
+  //stepper 4
+  pinMode(dir4cock, OUTPUT);
+  pinMode(step4cock, OUTPUT);
+  pinMode(MS14, OUTPUT);
+  pinMode(MS24, OUTPUT);
+  
+  // stepper 3
+  pinMode(dir3cock, OUTPUT);
+  pinMode(step3cock, OUTPUT);
+  pinMode(MS13, OUTPUT);
+  pinMode(MS23, OUTPUT);
 
+  // stepper 2
+  pinMode(dir2cock, OUTPUT);
+  pinMode(step2cock, OUTPUT);
+  pinMode(MS12, OUTPUT);
+  pinMode(MS22, OUTPUT);
+
+  // set to 1/16 microstep
+  digitalWrite(MS12, HIGH);
+  digitalWrite(MS22, HIGH);  
+  digitalWrite(MS13, HIGH);
+  digitalWrite(MS23, HIGH);
+  digitalWrite(MS14, HIGH);
+  digitalWrite(MS24, HIGH);
+
+  // Initialize servos
+  fire.attach(servo1);
+  drum.attach(servo2);
+  
+  Serial.begin(115200);
+  print_instructions();
+}
+
+void print_instructions() {
+  Serial.println("ASV 3.0 Ball Shooter:");
+  Serial.println("1: Cock motors");
+  Serial.println("2: Reset cocking motors");
+  Serial.println("3: Reload drum");
+  Serial.println("4: Fire");
+  Serial.println("5: Toggle drum state");
+}
+
+void cock_motors() {  
+    digitalWrite(dir2cock, HIGH);
+    digitalWrite(dir3cock, HIGH);
+    for (int i = 0; i < stepsPerRevolution * microstep * 5; i++) {
+      digitalWrite(step3cock, HIGH);
+      digitalWrite(step4cock, HIGH);
+      delayMicroseconds(stepperdelay);
+      digitalWrite(step3cock, LOW);
+      digitalWrite(step4cock, LOW);
+      delayMicroseconds(stepperdelay);
+    }
+}
+
+void reset_motors() {
+    digitalWrite(dir2cock, LOW);
+    digitalWrite(dir3cock, LOW);
+    for (int i = 0; i < stepsPerRevolution * microstep * 5; i++) {
+      digitalWrite(step3cock, HIGH);
+      digitalWrite(step4cock, HIGH);
+      delayMicroseconds(stepperdelay);
+      digitalWrite(step3cock, LOW);
+      digitalWrite(step4cock, LOW);
+      delayMicroseconds(stepperdelay);
+    }
+}
+
+void reload_drum() {
+    digitalWrite(dir2cock, LOW);
+    for (int i = 0; i < stepsPerRevolution * microstep * 0.25; i++) {
+      digitalWrite(step2cock, HIGH);
+      delayMicroseconds(stepperdelay);
+      digitalWrite(step2cock, LOW);
+      delayMicroseconds(stepperdelay);
+    }
+}
+
+void fire_ball() {
+  //fire.write();
+}
+
+bool toggledrum() {
+  if (is_locked) {
+//    drum.write();     // unlock
+    is_locked = false;
+  } else {
+//    drum.write();     // lock
+    is_locked = true;
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+    if (Serial.available() > 0) {
+      int val = Serial.parseInt();
+      switch(val) {
+        case 1: {
+          Serial.println("Cocking motors...");
+          cock_motors();
+          print_instructions();
+          break;
+        }
+        case 2: {
+          Serial.println("Resetting cocking motors...");
+          reset_motors();
+          print_instructions();
+          break;
+        }
+        case 3: {
+          Serial.println("Reloading Drum...");
+          reload_drum();
+          print_instructions();
+          break;
+        }
+        case 4: {
+          Serial.println("FIREEEEE");
+          fire_ball();
+          print_instructions();
+          break;
+        }
+        case 5: {
+          Serial.println("Toggling Drum State...");
+          toggledrum();
+          print_instructions();
+          break;
+        }
+        default:
+          break;
+      }
+    }
+    delay(100);
 }
---
+++ ASV_ballshooter_1.0/define.h
@@ -0,0 +1,38 @@
+//===========================================
+//
+//    DEFINES FOR ASV 3 BALLSHOOTER 
+//
+//===========================================
+
+#ifndef _DEFINES_H
+#define _DEFINES_H
+
+// stepper 2 
+#define dir2cock 5
+#define step2cock 4
+#define MS12 25
+#define MS22 26
+#define stepsPerRevolution 200
+
+// stepper 3
+#define dir3cock 7
+#define step3cock 6
+#define MS13 28
+#define MS23 29
+
+// stepper 4 
+#define dir4cock 8
+#define step4cock 9
+#define MS14 31
+#define MS24 32
+
+// steppers 
+#define microstep 16
+#define stepperdelay 200
+
+// servos
+#define servo1 12
+#define servo2 12
+#define servo3 14
+
+#endif // _DEFINES_H
